7e27a641a044d33be2fd18dc7df6ae25cdea9e8d
MJJH <martijnvriens94@live.nl>
2015-10-19 13:59:07 +0200

    CharacterType implemented

--- TheGame/src/thegame/com/Game/Objects/Characters/CharacterType.java
+++ TheGame/src/thegame/com/Game/Objects/Characters/CharacterType.java
@@ -2,17 +2,42 @@ package thegame.com.Game.Objects.Characters;
 
 import thegame.com.Game.Objects.*;
 import java.util.List;
+import java.util.Map;
 
 /**
  *
  * @author laure
  */
-public enum CharacterType {
+public class CharacterType {
 
-    ;
+    /** 
+     * Give the name of the character
+     * @Return name
+     */
+    public final String name;
 
-	Character characterType;
-    private String name;
-    private List<MapObject> item;
+    /**
+     * Give the starting items of the character
+     * @Return startItems
+     */
+    public final List<MapObject> startItems;
+
+    /**
+     * Give the starting skill levels of the character
+     * @Return startSkills
+     */
+    public final Map<SkillType, Integer> startSkills;
+    
+    /**
+     * Chreate a new charactertype with a name and starting options
+     * @param name
+     * @param startItems
+     * @param startSkills
+     */
+    public CharacterType(String name, List<MapObject> startItems, Map<SkillType, Integer> startSkills) {
+        this.name = name;
+        this.startItems = startItems;
+        this.startSkills = startSkills;
+    }
 
 }
