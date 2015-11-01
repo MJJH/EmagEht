2a2c395a738dcbb06ecf8c4668df1b7b37030a06
LaurensAdema <laurensadema@live.nl>
2015-11-01 15:10:17 +0100

    Dun goofed

--- TheGame/src/thegame/com/Game/Objects/Block.java
+++ TheGame/src/thegame/com/Game/Objects/Block.java
@@ -1,7 +1,5 @@
 package thegame.com.Game.Objects;
 
-import com.sun.javafx.scene.traversal.Direction;
-import javafx.scene.image.Image;
 import thegame.com.Game.Map;
 
 /**
@@ -15,6 +13,7 @@ public class Block extends MapObject {
 
     /**
      * Initiates this class
+     *
      * @param type The type of the block
      * @param x the X-coordinate of its location
      * @param y the Y- coordinate of its location
@@ -26,22 +25,26 @@ public class Block extends MapObject {
         super(x, y, type.skin, 1.0f, 1.0f, solid, map);
         this.type = type;
     }
-    
+
     /**
      *
      * @param tool
      * @return
      */
-    public int hitBlock (Tool tool){
+    public int hitBlock(Tool tool)
+    {
         return 0;
     }
 
     @Override
-    public void update() {}
+    public void update()
+    {
+    }
 
     @Override
-    public void hit(Tool use, Direction hitDirection) {
+    public void hit(Tool use, sides hitDirection)
+    {
         System.err.println("Found: " + this.type.name);
     }
-    
+
 }
--- TheGame/src/thegame/com/Game/Objects/Tool.java
+++ TheGame/src/thegame/com/Game/Objects/Tool.java
@@ -1,11 +1,10 @@
 package thegame.com.Game.Objects;
 
-import com.sun.javafx.scene.traversal.Direction;
-import javafx.scene.image.Image;
 import thegame.com.Game.Map;
 
 /**
  * A tool is an object that a character can use to mine, fight, dig or chop
+ *
  * @author Martijn
  */
 public class Tool extends MapObject {
@@ -14,8 +13,9 @@ public class Tool extends MapObject {
 
     /**
      * Create a usable tool
-     * @param type      the type of this tool (sword, hatchet, etc)
-     * @param map       the map this tool is used on
+     *
+     * @param type the type of this tool (sword, hatchet, etc)
+     * @param map the map this tool is used on
      */
     public Tool(ToolType type, Map map)
     {
@@ -24,9 +24,13 @@ public class Tool extends MapObject {
     }
 
     @Override
-    public void update() {}
-    
+    public void update()
+    {
+    }
+
     @Override
-    public void hit(Tool use, Direction hitDirection) {}
+    public void hit(Tool use, sides hitDirection)
+    {
+    }
 
 }
