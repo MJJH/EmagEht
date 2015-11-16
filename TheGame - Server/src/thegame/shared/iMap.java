e2625ea555fa40b905716e68771ccc38313035d5
MJJH <martijnvriens94@live.nl>
2015-11-16 14:31:10 +0100

    Interface Map

---
+++ TheGame - Server/src/thegame/shared/iMap.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package thegame.shared;
+
+import java.rmi.Remote;
+import java.util.List;
+import thegame.com.Game.Objects.MapObject;
+
+/**
+ *
+ * @author Martijn
+ */
+public interface iMap extends Remote {
+    
+    public List<MapObject> getObjects(int startX, int startY, int endX, int endY);
+    
+    public int getHeight();
+    
+    public int getWidth();
+    
+}
